a88149c7820eeaf85da668383ab78363b02cc38c
VadimPonomarov <pvs.versia@gmail.com>
2022-09-05 11:21:36 +0300

    lesson_4

---
+++ apps/users/models.py
@@ -0,0 +1,23 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+from apps.users.managers import UserManager
+
+
+class UserModel(AbstractUser):
+    class Meta:
+        db_table = 'auth_user'
+
+    email = models.EmailField(unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
